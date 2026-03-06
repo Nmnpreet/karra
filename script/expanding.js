8bb34440f2e42541901ffb457c01d6039b01cd3d
Nmnpreet <nmnpreet25@gmail.com>
2026-03-06 03:55:44 +0000

    Thu, Mar 5, 2026, 7:55 PM -08:00

--- script/expanding.js
+++ script/expanding.js
@@ -1,9 +1,9 @@
-panels= document.querySelectorAll('.panel');
-for (i = 0; i < panels.length i = i + 1)
+const panels = document.querySelectorAll('.panel');
+for (let i = 0; i < panels.length i = i + 1)
 {
 
-panels[i].addEventListener('click',function ()  {
-    removeActiveFromAllPanels()
+panels[i].addEventListener('click', function ()  {
+    removeActiveFromAllPanels();
 panels[i].classList.add('active');
 });
 }

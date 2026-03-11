ebfa96d4bce2b186d2ba5e58a3d153c9063bc646
Nmnpreet <nmnpreet25@gmail.com>
2026-03-11 16:48:29 +0000

    Wed, Mar 11, 2026, 9:48 AM -07:00

--- script/sound.js
+++ script/sound.js
@@ -5,26 +5,11 @@ boo = document.querySelector('.boo')
 
 btn = document.createElement('button')
 btn.innerText = "applause"
-btn.classlist.add('btn')
+btn.classList.add('btn')
 
 btn.addEventListener('click', () =>{
 applause.play()
 })
 buttons.appendChild(btn)
-
-
-buttons = document.querySelector('.buttons')
-applause = document.querySelector('.applause')
-
-
-btn = document.createElement('.button')
-btn.innerText = "applause"
-btn.classlist.add('.btn')
-
-btn.addEventListener('click', () =>{
-applause.play()
-})
-buttons.appendChild(btn)
-
 
 

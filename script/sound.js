3b244b8a83f9b2e707b68e4866687d623abbe646
Nmnpreet <nmnpreet25@gmail.com>
2026-03-12 19:54:12 +0000

    Thu, Mar 12, 2026, 12:54 PM -07:00

--- script/sound.js
+++ script/sound.js
@@ -1,6 +1,7 @@
 buttons = document.querySelector('.buttons')
 sounds = ['applause','boo','gasp','tada','victory','wrong']
 
+
 for (let i = 0; i < 6; i = i + 1){
     btn = document.createElement('button')
     btn.innerText = sounds[i]

40b3e1820b1e20fd06845f5664e42b0f999d4f65
Nmnpreet <nmnpreet25@gmail.com>
2026-03-23 05:14:08 +0000

    Sun, Mar 22, 2026, 10:14 PM -07:00

--- script/insect.js
+++ script/insect.js
@@ -1,10 +1,11 @@
 const screens = document.querySelectorAll('.screen')
-screen[0].classList.add('up')
 
-
-
-// const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
-// const start_btn = document.getElementById('start-btn')
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
+const game_container = document.getElementById('game-container')
+const start-btn = document.getElementById('start-btn')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
 
 
 // start_btn.addEventListener('click', () => {

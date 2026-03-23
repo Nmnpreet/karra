e8462309a2c09b622b2156e2753b369e28c89f43
Nmnpreet <nmnpreet25@gmail.com>
2026-03-23 06:01:01 +0000

    Sun, Mar 22, 2026, 11:01 PM -07:00

--- script/insect.js
+++ script/insect.js
@@ -2,13 +2,13 @@ const screens = document.querySelectorAll('.screen')
 
 const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
 const game_container = document.getElementById('game-container')
-const start-btn = document.getElementById('start-btn')
+const start_btn = document.getElementById('start-btn')
 const timeEl = document.getElementById('time')
 const scoreEl = document.getElementById('score')
 const message = document.getElementById('message')
 let seconds = 0
 let score = 0
-let selected_insect {}
+let selected_insect={}
 
 start_btn.addEventListener('click', () => {
     screens[0].classList.add('up')
@@ -20,7 +20,7 @@ choose_insect_btns.forEach(btn => {
         const alt = img.getAttribute('alt')
         const src = img.getAttribute('src')
         screens[1].classList.add('up')
-        selected_insected = {src, alt}
+        selected_insect = {src, alt}
         setTimeout(createInsect, 1000)
         startGame()
 

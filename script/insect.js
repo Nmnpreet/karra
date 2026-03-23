40462fb9b2152ed7c1976df03f74f9997fcfa7cd
Nmnpreet <nmnpreet25@gmail.com>
2026-03-23 05:09:33 +0000

    Sun, Mar 22, 2026, 10:09 PM -07:00

--- script/insect.js
+++ script/insect.js
@@ -1,47 +1,51 @@
 const screens = document.querySelectorAll('.screen')
-const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
-const start_btn = document.getElementById('start-btn')
+screen[0].classList.add('up')
 
 
-start_btn.addEventListener('click', () => {
-    screens[0].classList.add('up')
-})
 
-for (let i = 0; i < choose_insect_btns.length; i++) {
-    choose_insect_btns[i].addEventListener('click', () => {
-        screens[1].classList.add('up')
+// const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
+// const start_btn = document.getElementById('start-btn')
 
-        const img = choose_insect_btns[i].querySelector('img')
-        const src = img.getAttribute('src')
-        const alt = img.getAttribute('alt')
 
-        startGame()
-        createInsect()
-    })
-}
+// start_btn.addEventListener('click', () => {
+//     screens[0].classList.add('up')
+// })
 
-function startGame() {
-    setTimeout(createInsect, 1000)
-}
+// for (let i = 0; i < choose_insect_btns.length; i++) {
+//     choose_insect_btns[i].addEventListener('click', () => {
+//         screens[1].classList.add('up')
 
-function createInsect() {
-    console.log("hello")
-    const insect = document.createElement('div')
-    insect.classList.add('insect')
-    {x,y} = getRandomLocation()
-    insect.style.top = `${y}px`
-    insect.style.left = `${y}px`
+//         const img = choose_insect_btns[i].querySelector('img')
+//         const src = img.getAttribute('src')
+//         const alt = img.getAttribute('alt')
 
-    insect.innerHTML = `<img src="roach.png" alt="roach">` // FIX: syntax
-    document.body.appendChild(insect)
-}
+//         startGame()
+//         createInsect()
+//     })
+// }
 
-function getRandomLocation()
-{
-    width = window.insectwidth
-    height= window.innerheight
-    x = Math.random()*(width-200)+100
-    y = Math.random()*(height-200)+100
-    return {x,y}
+// function startGame() {
+//     setTimeout(createInsect, 1000)
+// }
 
-}
+// function createInsect() {
+//     console.log("hello")
+//     const insect = document.createElement('div')
+//     insect.classList.add('insect')
+//     {x,y} = getRandomLocation()
+//     insect.style.top = `${y}px`
+//     insect.style.left = `${y}px`
+
+//     insect.innerHTML = `<img src="roach.png" alt="roach">` // FIX: syntax
+//     document.body.appendChild(insect)
+// }
+
+// function getRandomLocation()
+// {
+//     width = window.insectwidth
+//     height= window.innerheight
+//     x = Math.random()*(width-200)+100
+//     y = Math.random()*(height-200)+100
+//     return {x,y}
+
+// }

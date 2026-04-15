a9150617ebbc47ea97ee63d9c5692444aff837e7
Nmnpreet <nmnpreet25@gmail.com>
2026-04-15 16:23:39 +0000

    Wed, Apr 15, 2026, 9:23 AM -07:00

--- script/ball.js
+++ script/ball.js
@@ -30,7 +30,7 @@ function  createBall()
     ball.style.height = `${2 * ballRadius}px`
     ball.style.width =  `${2 * ballRadius}px`
     ball.style.borderRadius = "50%"
-    ball.style.backgroundColor = "blue"
+    ball.style.backgroundColor = "green"
     ball.style.position = "absolute"
     ball.style.top = `${windowHeight/2 - ballRadius}px`
     ball.style.left = `${ballXPosition}px`

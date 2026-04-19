2d5ac60348808e713839d5f7e60a2d90cde0440e
Nmnpreet <nmnpreet25@gmail.com>
2026-04-19 03:48:53 +0000

    Sat, Apr 18, 2026, 8:48 PM -07:00

--- script/relax.js
+++ script/relax.js
@@ -1,30 +1,58 @@
-const conatiner = document.getElementById('container')
+// const conatiner = document.getElementById('container')
+// const text = document.getElementById('text')
+
+// // totaltime = 7500
+// // breatheTime = (totalTime / 5 )* 2
+// // HoldTime = totalTime / 5
+
+// setInterval(breatheAnimation, 7500)
+
+// function breatheAnimation()
+// {
+//     text.innerText = 'Breathe In'
+//     container.classList.remove('shrink')
+//     container.classList.add('grow')
+
+//     // setTimeout( () => {
+//     //     text.innerText= 'hold'
+
+
+//     // },3000)
+
+//     setTimeout( () => {
+//                 text.innerText= 'hold'
+
+//         text.innerText = 'Breathe Out!'
+//         conatiner.clasList.remove('grow')
+//         container.classList.add('shrink')
+//     },4500)
+//  }
+
+const container = document.getElementById('container')
 const text = document.getElementById('text')
 
-// totaltime = 7500
-// breatheTime = (totalTime / 5 )* 2
-// HoldTime = totalTime / 5
+const totalTime = 7500
+const breatheTime = (totalTime / 5) * 2   // 3000ms
+const holdTime = totalTime / 5            // 1500ms
 
-setInterval(breatheAnimation, 7500)
+breatheAnimation()
+setInterval(breatheAnimation, totalTime)
 
-function breatheAnimation()
-{
+function breatheAnimation() {
+    // Breathe In
     text.innerText = 'Breathe In'
     container.classList.remove('shrink')
     container.classList.add('grow')
 
-    // setTimeout( () => {
-    //     text.innerText= 'hold'
+    // Hold
+    setTimeout(() => {
+        text.innerText = 'Hold'
+    }, breatheTime)
 
-
-    // },3000)
-
-    setTimeout( () => {
-                text.innerText= 'hold'
-
-        text.innerText = 'Breathe Out!'
-        conatiner.clasList.remove('grow')
+    // Breathe Out
+    setTimeout(() => {
+        text.innerText = 'Breathe Out'
+        container.classList.remove('grow')
         container.classList.add('shrink')
-    },4500)
- }
-
+    }, breatheTime + holdTime)
+}

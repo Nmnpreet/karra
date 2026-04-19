1477b975d5b17e997a26f2053d6b0c3479dfc8e4
Nmnpreet <nmnpreet25@gmail.com>
2026-04-19 03:50:43 +0000

    Sat, Apr 18, 2026, 8:50 PM -07:00

--- script/relax.js
+++ script/relax.js
@@ -1,33 +1,3 @@
-// const conatiner = document.getElementById('container')
-// const text = document.getElementById('text')
-
-// // totaltime = 7500
-// // breatheTime = (totalTime / 5 )* 2
-// // HoldTime = totalTime / 5
-
-// setInterval(breatheAnimation, 7500)
-
-// function breatheAnimation()
-// {
-//     text.innerText = 'Breathe In'
-//     container.classList.remove('shrink')
-//     container.classList.add('grow')
-
-//     // setTimeout( () => {
-//     //     text.innerText= 'hold'
-
-
-//     // },3000)
-
-//     setTimeout( () => {
-//                 text.innerText= 'hold'
-
-//         text.innerText = 'Breathe Out!'
-//         conatiner.clasList.remove('grow')
-//         container.classList.add('shrink')
-//     },4500)
-//  }
-
 const container = document.getElementById('container')
 const text = document.getElementById('text')
 
